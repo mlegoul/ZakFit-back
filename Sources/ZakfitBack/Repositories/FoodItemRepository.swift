875de4364bc8e5327a2ca328ed41a5bcd3880bcf
Mehdi <legoullonmehdi@gmail.com>
2025-12-02 15:51:07 +0100

    Finish create Meal

--- Sources/ZakfitBack/Repositories/FoodItemRepository.swift
+++ Sources/ZakfitBack/Repositories/FoodItemRepository.swift
@@ -11,6 +11,13 @@ import Vapor
 protocol FoodItemRepositoryProtocol: Sendable {
     func createFoodItem(_ foodItem: FoodItem, on db: any Database) async throws
     func getFoodItemByName(_ name: String, on db: any Database) async throws -> FoodItem?
+    func createMeal(
+        userId: UUID,
+        type: String,
+        foodItems: [CreateMealFoodItemRequest],
+        date: Date,
+        on db: any Database
+    ) async throws -> MealResponse
 }
 
 struct FoodItemRepository: FoodItemRepositoryProtocol {
@@ -22,5 +29,112 @@ struct FoodItemRepository: FoodItemRepositoryProtocol {
         try await FoodItem.query(on: db)
             .filter(\.$name == name)
             .first()
+    }
+    
+    func createMeal(
+        userId: UUID,
+        type: String,
+        foodItems: [CreateMealFoodItemRequest],
+        date: Date,
+        on db: any Database
+    ) async throws -> MealResponse {
+
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: date)
+        let endOfDay = calendar.date(
+            bySettingHour: 23,
+            minute: 59,
+            second: 59,
+            of: date
+        )!
+        
+        if let existingMeal = try await Meal.query(on: db)
+            .filter(\.$user.$id == userId)
+            .filter(\.$type == type)
+            .filter(\.$date >= startOfDay)
+            .filter(\.$date <= endOfDay)
+            .first() {
+            
+            var totalCalories = Int(existingMeal.calories)
+            
+            for foodItemRequest in foodItems {
+                let lowercasedName = foodItemRequest.name.lowercased()
+                guard let existingFoodItem = try await getFoodItemByName(lowercasedName, on: db) else {
+                    throw Abort(
+                        .notFound,
+                        reason: "Aliment non trouvé en base de données : \(foodItemRequest.name)"
+                    )
+                }
+                
+                let calories = Int(
+                    existingFoodItem.calories * Double(
+                        foodItemRequest.consumedQuantity
+                    ) / 100.0
+                )
+                totalCalories += calories
+                
+                let mealFoodItem = MealFoodItem(
+                    mealId: try existingMeal.requireID(),
+                    foodItemId: try existingFoodItem.requireID(),
+                    consumedQuantity: foodItemRequest.consumedQuantity
+                )
+                try await mealFoodItem.save(on: db)
+            }
+            
+            existingMeal.calories = totalCalories
+            try await existingMeal.update(on: db)
+            
+            return MealResponse(
+                id: try existingMeal.requireID(),
+                type: existingMeal.type,
+                calories: existingMeal.calories,
+                date: existingMeal.date
+            )
+        } else {
+
+            let meal = Meal(
+                userId: userId,
+                type: type,
+                calories: 0,
+                date: date
+            )
+            
+            try await meal.save(on: db)
+            var totalCalories = 0
+            
+            for foodItemRequest in foodItems {
+                let lowercasedName = foodItemRequest.name.lowercased()
+                guard let existingFoodItem = try await getFoodItemByName(lowercasedName, on: db) else {
+                    throw Abort(
+                        .notFound,
+                        reason: "Aliment non trouvé en base de données : \(foodItemRequest.name)"
+                    )
+                }
+                
+                let calories = Int(
+                    existingFoodItem.calories * Double(
+                        foodItemRequest.consumedQuantity
+                    ) / 100.0
+                )
+                totalCalories += calories
+                
+                let mealFoodItem = MealFoodItem(
+                    mealId: try meal.requireID(),
+                    foodItemId: try existingFoodItem.requireID(),
+                    consumedQuantity: foodItemRequest.consumedQuantity
+                )
+                try await mealFoodItem.save(on: db)
+            }
+            
+            meal.calories = totalCalories
+            try await meal.update(on: db)
+            
+            return MealResponse(
+                id: try meal.requireID(),
+                type: meal.type,
+                calories: meal.calories,
+                date: meal.date
+            )
+        }
     }
 }
--- Sources/ZakfitBack/Services/FoodItemService.swift
+++ Sources/ZakfitBack/Services/FoodItemService.swift
@@ -30,73 +30,49 @@ struct FoodItemService {
         date: Date,
         on db: any Database
     ) async throws -> MealResponse {
-        let meal = Meal(
+        for foodItemRequest in foodItems {
+            let lowercasedName = foodItemRequest.name.lowercased()
+            if try await foodItemRepository
+                .getFoodItemByName(lowercasedName, on: db) == nil {
+                let foodItemResponse = try await fetchFoodItemData(
+                    name: foodItemRequest.name
+                )
+                let newFoodItem = FoodItem(
+                    name: foodItemResponse.name.lowercased(),
+                    referenceQuantity: 100,
+                    unit: "g",
+                    calories: foodItemResponse.calories,
+                    proteins: foodItemResponse.proteins,
+                    carbs: foodItemResponse.carbs,
+                    fats: foodItemResponse.fats
+                )
+                try await foodItemRepository.createFoodItem(newFoodItem, on: db)
+            }
+        }
+        
+        return try await foodItemRepository.createMeal(
             userId: userId,
             type: type,
-            calories: 0,
-            date: date
+            foodItems: foodItems,
+            date: date,
+            on: db
         )
-        
-        return try await db.transaction { db in
-            try await meal.save(on: db)
-            var totalCalories = 0
-            
-            for foodItemRequest in foodItems {
-                let lowercasedName = foodItemRequest.name.lowercased()
-                if let existingFoodItem = try await foodItemRepository.getFoodItemByName(lowercasedName, on: db) {
-                    let calories = Int(existingFoodItem.calories * Double(foodItemRequest.consumedQuantity) / 100.0)
-                    totalCalories += calories
-                    let mealFoodItem = MealFoodItem(
-                        mealId: try meal.requireID(),
-                        foodItemId: try existingFoodItem.requireID(),
-                        consumedQuantity: foodItemRequest.consumedQuantity
-                    )
-                    try await mealFoodItem.save(on: db)
-                } else {
-                    print("Aliment non trouvé en base, appel API pour : \(foodItemRequest.name)")
-                    let foodItemResponse = try await fetchFoodItemData(name: foodItemRequest.name)
-                    let newFoodItem = FoodItem(
-                        name: foodItemResponse.name.lowercased(),
-                        referenceQuantity: 100,
-                        unit: "g",
-                        calories: foodItemResponse.calories,
-                        proteins: foodItemResponse.proteins,
-                        carbs: foodItemResponse.carbs,
-                        fats: foodItemResponse.fats
-                    )
-                    try await foodItemRepository.createFoodItem(newFoodItem, on: db)
-                    let calories = Int(foodItemResponse.calories * Double(foodItemRequest.consumedQuantity) / 100.0)
-                    totalCalories += calories
-                    let mealFoodItem = MealFoodItem(
-                        mealId: try meal.requireID(),
-                        foodItemId: try newFoodItem.requireID(),
-                        consumedQuantity: foodItemRequest.consumedQuantity
-                    )
-                    try await mealFoodItem.save(on: db)
-                }
-            }
-            
-            meal.calories = totalCalories
-            try await meal.update(on: db)
-            
-            return MealResponse(
-                id: try meal.requireID(),
-                type: meal.type,
-                calories: meal.calories,
-                date: meal.date
-            )
-        }
     }
-    
+
     func fetchFoodItemData(name: String) async throws -> FoodItemResponse {
-        let encodedFoodName = name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let encodedFoodName = name.addingPercentEncoding(
+            withAllowedCharacters: .urlQueryAllowed
+        ) ?? ""
         let urlString = "https://world.openfoodfacts.org/cgi/search.pl?search_terms=\(encodedFoodName)&search_simple=1&action=process&json=1&fields=product_name,nutriments"
         let url = URI(string: urlString)
         
         let response = try await client.get(url)
         
         guard response.status == .ok else {
-            throw Abort(.badRequest, reason: "Erreur lors de la récupération des données de l'API Open Food Facts")
+            throw Abort(
+                .badRequest,
+                reason: "Erreur lors de la récupération des données de l'API Open Food Facts"
+            )
         }
         
         let foodData = try response.content.decode(OpenFoodFactsResponse.self)
